c1797614f84f8387c15527826cc40f850e8aacd6
ruipan <panrui@knowbox.cn>
2019-05-17 19:59:33 +0800

    dev

--- http/front.go
+++ http/front.go
@@ -285,7 +285,7 @@ func customerWxPayDeposit(c *gin.Context) {  //微信支付定金
 
 
 	db := mysql.GetInstance(false)
-	orderId := db.Insert(records.RecordNameNormalOrder).Columns("customer_id", "mobile", "name", "product_id", "valid", "withdraw").
+	orderId := db.Insert(records.RecordNameNormalOrder).Columns("customer_id", "mobile", "name", "product_id", "payed", "withdraw").
 		Value(customerId, req.Mobile, "", req.ProductId, 0, 0).Execute().LastInsertId()
 	params, err := methods.UnifiedOrder(conf.Config.Wx, strconv.Itoa(orderId), c.ClientIP(), openid)
 	if err != nil {
